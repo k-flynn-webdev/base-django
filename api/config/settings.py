5c1ead860ecf3cc3c6f7e5e08a7c09161f617209
kevin flynn <flynny85@gmail.com>
2021-05-25 19:13:25 +0100

    add new tests

--- api/config/settings.py
+++ api/config/settings.py
@@ -196,8 +196,7 @@ REST_AUTH_SERIALIZERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
+        'NAME': BASE_DIR / 'db.sqlite3'    }
 }
 
 
--- api/csrf/views.py
+++ api/csrf/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from django.middleware.csrf import get_token
-
-# Create your views here.
+from django.http import JsonResponse
 
 
 def get_csrf(request):
