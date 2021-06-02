72d3a15ccbcef1ce6cd90b51dec1f20a1b7795e6
kevin flynn <flynny85@gmail.com>
2021-06-02 13:11:24 +0100

    add vue config env vars now working

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,30 +1,28 @@
+require('dotenv-expand')(require('dotenv').config({ path: `../.env` }))
+
 process.env.VUE_APP_VERSION = require('./package.json').version
-process.env.VUE_APP_NAME = require('./package.json').name
+process.env.VUE_APP_NAME = process.env.APP_NAME_FULL
 
 module.exports = {
   devServer: {
     proxy: {
       '^/static': {
-        // target: process.env.API_ADDRESS,
-        target: 'http://127.0.0.1:8700',
+        target: process.env.API_ADDRESS || 'http://127.0.0.1:8700',
         changeOrigin: true,
         ws: false,
       },
       '^/admin': {
-        // target: process.env.API_ADDRESS,
-        target: 'http://127.0.0.1:8700',
+        target: process.env.API_ADDRESS || 'http://127.0.0.1:8700',
         changeOrigin: true,
         ws: false,
       },
       '^/accounts': {
-        // target: process.env.URL_ACCOUNTS,
-        target: 'http://127.0.0.1:8700',
+        target: process.env.API_ADDRESS || 'http://127.0.0.1:8700',
         changeOrigin: true,
         ws: false,
       },
       '^/api': {
-        // target: process.env.API_ADDRESS,
-        target: 'http://127.0.0.1:8700',
+        target: process.env.API_ADDRESS || 'http://127.0.0.1:8700',
         changeOrigin: true,
         ws: false,
       },
